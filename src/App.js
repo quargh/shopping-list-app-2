1af4bcfbac4cdd7ccbf69bea40c05ceaff14ccea
Meral Sager <mersa22@gmx.de>
2022-08-05 20:43:46 +0200

    Add useEffect to App.js

--- src/App.js
+++ src/App.js
@@ -5,63 +5,21 @@ import SearchInput from "./Components/SearchInput";
 
 export default function App() {
   const url = "https://fetch-me.vercel.app/api/shopping/items";
+
+  // Mit useEffectin Kombination [] haben wir die Möglichkeit etwas 1x(Zeile 11 fetchdata) durchzuführen .
+  useEffect(() => {
+    fetchdata(url);
+  }, []);
+  // Hier definieren wir was passieren soll.Definieren der Funktion
+  async function fetchdata(url) {
+    // Daten holen von url
+    const response = await fetch(url);
+    // Hier werden die url daten in json parsen (umwandeln/konvertiert).
+    const data = response.json();
+  }
+
   const [item, setItem] = useState("");
-  const [list, setList] = useState([
-    {
-      _id: "c2hvcHBpbmcuaXRlbTox",
-      _type: "shopping.item",
-      category: { _type: "ref", _ref: "c2hvcHBpbmcuY2F0ZWdvcnk6MA==" },
-      name: { en: "Pineapple", de: "Ananas" },
-    },
-    {
-      _id: "c2hvcHBpbmcuaXRlbToy",
-      _type: "shopping.item",
-      category: { _type: "ref", _ref: "c2hvcHBpbmcuY2F0ZWdvcnk6MA==" },
-      name: { en: "Apples", de: "Äpfel" },
-    },
-    {
-      _id: "c2hvcHBpbmcuaXRlbToz",
-      _type: "shopping.item",
-      category: { _type: "ref", _ref: "c2hvcHBpbmcuY2F0ZWdvcnk6MA==" },
-      name: { en: "Apricots", de: "Aprikosen" },
-    },
-    {
-      _id: "c2hvcHBpbmcuaXRlbTo0",
-      _type: "shopping.item",
-      category: { _type: "ref", _ref: "c2hvcHBpbmcuY2F0ZWdvcnk6MA==" },
-      name: { en: "Artichokes", de: "Artischocken" },
-    },
-    {
-      _id: "c2hvcHBpbmcuaXRlbTo1",
-      _type: "shopping.item",
-      category: { _type: "ref", _ref: "c2hvcHBpbmcuY2F0ZWdvcnk6MA==" },
-      name: { en: "Aubergine", de: "Aubergine" },
-    },
-    {
-      _id: "c2hvcHBpbmcuaXRlbTo2",
-      _type: "shopping.item",
-      category: { _type: "ref", _ref: "c2hvcHBpbmcuY2F0ZWdvcnk6MA==" },
-      name: { en: "Oyster mushrooms", de: "Austernpilze" },
-    },
-    {
-      _id: "c2hvcHBpbmcuaXRlbTo3",
-      _type: "shopping.item",
-      category: { _type: "ref", _ref: "c2hvcHBpbmcuY2F0ZWdvcnk6MA==" },
-      name: { en: "Avocado", de: "Avocado" },
-    },
-    {
-      _id: "c2hvcHBpbmcuaXRlbTo4",
-      _type: "shopping.item",
-      category: { _type: "ref", _ref: "c2hvcHBpbmcuY2F0ZWdvcnk6MA==" },
-      name: { en: "Baby spinach", de: "Babyspinat" },
-    },
-    {
-      _id: "c2hvcHBpbmcuaXRlbTo5",
-      _type: "shopping.item",
-      category: { _type: "ref", _ref: "c2hvcHBpbmcuY2F0ZWdvcnk6MA==" },
-      name: { en: "Bananas", de: "Bananen" },
-    },
-  ]);
+  const [list, setList] = useState([]);
 
   const handleChange = () => {};
 
